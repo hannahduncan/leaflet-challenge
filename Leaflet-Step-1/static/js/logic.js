a23f99a0313fe187dee5151ecb30bbe76844f2d5
Hannah <hannah@Hannahs-MacBook-Pro.local>
2020-11-17 20:07:05 -0600

    update

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -16,11 +16,6 @@ var mapLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 d3.json(url, function(data) {
-    // Popups not showing up
-    var earthquakes = L.geoJSON(data.features, {
-        onEachFeature: function(feature, layer) {
-            layer.bindPopup(`<h3> ${feature.properties.mag} </h3>`)   
-    }});
 
     function colorPicker(depth) {
         if (depth<5) {
@@ -35,72 +30,55 @@ d3.json(url, function(data) {
         return color
     };
 
-    // try {
-        var features = data.features
-        var mags = []
-        var coordinates = []
-        var depths = []
+    var features = data.features
+    var mags = []
+    var coordinates = []
+    var depths = []
 
-        for (var i=0; i<features.length; i++) {
-            mags.push(features[i].properties.mag)
-            coordinates.push(features[i].geometry.coordinates.slice(0,2).reverse())
-            depths.push(features[i].geometry.coordinates[2])
-        }
+    for (var i=0; i<features.length; i++) {
+        mags.push(features[i].properties.mag)
+        coordinates.push(features[i].geometry.coordinates.slice(0,2).reverse())
+        depths.push(features[i].geometry.coordinates[2])
+    }
 
-        for (var i=0; i<coordinates.length; i++) {
-                L.circle(coordinates[i], {
-                color: colorPicker(depths[i]),
-                fillColor: colorPicker(depths[i]),
-                // Pass function to pick color for depth
-                // If depth within range, choose that color
-                fillOpacity: .8,
-                radius: mags[i] * 10000
-            }).addTo(map);
-        }
-        // Set up the legend
-        var legend = L.control({ position: "bottomright" });
-        legend.onAdd = function() {
-            var div = L.DomUtil.create("div", "info legend");
-            // var limits = L.geoJson.options.limits;
-            // console.log(limits)
-            var colors = ["green","yellow","red"]
-            var labels = [];
+    for (var i=0; i<coordinates.length; i++) {
+            L.circle(coordinates[i], {
+            color: colorPicker(depths[i]),
+            fillColor: colorPicker(depths[i]),
+            fillOpacity: .8,
+            radius: mags[i] * 10000
+        }).bindPopup(
+            `<p> Magnitude: ${mags[i]} <br>
+            Location: (${coordinates[i]}) <br>
+            Depth: ${depths[i]} </p>`).addTo(map);
+    };
+    // Set up the legend
+    var legend = L.control({ position: "bottomright" });
+    legend.onAdd = function() {
+        var div = L.DomUtil.create("div", "info legend");
+        var limits = [0, 5, 10];
+        var colors = ["green","yellow","red"]
+        var labels = [];
 
-            // Add min & max
-            var legendInfo = "<h1>Earthquake Depths</h1>" +
-            "<div class=\"labels\">" +
-                // "<div class=\"min\">" + limits[0] + "</div>" +
-                // "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-            "</div>";
+        // Add min & max
+        var legendInfo = "<h1>Earthquake Depths</h1>" +
+        "<div class=\"labels\">" +
+            "<div class=\"min\">" + limits[0] + "</div>" +
+            "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+        "</div>";
 
-            div.innerHTML = legendInfo;
+        div.innerHTML = legendInfo;
 
-            // limits.forEach(function(limit, index) {
-            colors.forEach(function(limit, index) {
-                labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-            });
+        limits.forEach(function(limit, index) {
+        // colors.forEach(function(limit, index) {
+            labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+        });
 
-            div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-            return div;
-        };
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        return div;
+    };
 
-        // Adding legend to the map
-        legend.addTo(map);
-
-        
-            
-        
-    // } catch {
-    //     console.log("Nope")
-    // }
+    // Adding legend to the map
+    legend.addTo(map);
     
-
-    var baseMap = {
-        default: mapLayer
-    };
-    var overlay = {
-        earthquakes: earthquakes
-    };
-
-    L.control.layers(baseMap,overlay).addTo(map)
 })
